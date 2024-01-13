6856f3bf141fa8913ecb2cf19cf032fdae7c6e66
PVA <you@example.com>
2024-01-14 02:25:10 +0800

    Исправленные ошибки по первому методу и печать массива

--- Program.cs
+++ Program.cs
@@ -10,12 +10,22 @@ string[] arr2 = new string[arr1.Length];
 void SelectArr(string[] arr1, string[] arr2)
 {
     int count = 0;
-    for (int i = 0; i < arr1.Length; i++);
+    for (int i = 0; i < arr1.Length; i++)
     {
-    if(arr1[i].Length <= 3);
+    if(arr1[i].Length <= 3)
         {
             arr2[count] = arr1[i];
             count++;
         }    
     }
 }
+void PrintArr(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine();
+}
+SelectArr(arr1, arr2);
+PrintArr(arr2);
