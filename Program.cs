022322bb4f59bda92730a85e0b9f35ebac965643
PVA <you@example.com>
2024-01-14 02:19:53 +0800

    Готовый метод по заданию

--- Program.cs
+++ Program.cs
@@ -3,5 +3,19 @@
 // длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется
 // пользоваться коллекциями, лучше обойтись исключительно массивами.
-string[] arr1 = string InputNum(string message)
-ConsoleWriteLine(arr)
+
+string[] arr1 = new string[5] {"привет", "32", "Who i am?", "Dog", "2+1=4"};
+string[] arr2 = new string[arr1.Length];
+
+void SelectArr(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++);
+    {
+    if(arr1[i].Length <= 3);
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }    
+    }
+}
